7a23adbabfab22f5b91d8cd3bf8e8d0633ef5556
NikolayNN <kolya.by@mail.ru>
2018-02-12 22:52:21 +0300

    refactored Props class.

--- src/main/java/utils/Props.java
+++ src/main/java/utils/Props.java
@@ -1,30 +1,28 @@
 package utils;
 
 import java.io.IOException;
+import java.util.Properties;
 
 public class Props {
 
     private static final String FILENAME = "app.properties";
+    private static final Properties properties;
 
-    public static String read(String propertyName) {
-        java.util.Properties properties = new java.util.Properties();
+    static {
+        properties = new Properties();
         ClassLoader loader = Props.class.getClassLoader();
         try {
             properties.load(loader.getResourceAsStream(FILENAME));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(String.format("Can't read property file: %s. %s", FILENAME, e.getMessage()));
         }
+    }
+
+    public static String read(String propertyName) {
         return properties.getProperty(propertyName);
     }
 
     public static int readInt(String propertyName) {
-        java.util.Properties properties = new java.util.Properties();
-        ClassLoader loader = Props.class.getClassLoader();
-        try {
-            properties.load(loader.getResourceAsStream(FILENAME));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         try {
             return Integer.parseInt(properties.getProperty(propertyName));
         } catch (Exception ex){
